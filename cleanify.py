ed2ac70313d076a2d1a5f75f4bd1be3570334271
Amila Ferron <aferron@pdx.edu>
2021-08-13 14:03:23 -0700

    Add distortion-specific naming for saved images and files, edit CNN

--- cleanify.py
+++ cleanify.py
@@ -153,9 +153,9 @@ class CleanerCNN(nn.Module):
             # for each pixel of the original (single layer) image for each channel layer.
             # Other parameter for Conv2d() are stride, padding_mode, dilation, groups, and bias.
             # It looks like conv2 is a bottleneck module?
-            self.conv1 = nn.Conv2d(3,  64, kernel_size=5, padding=1, bias=True)
+            self.conv1 = nn.Conv2d(3,  64, kernel_size=5, padding=2, bias=True)
             self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
-            self.conv3 = nn.Conv2d(32,  3, kernel_size=3, padding=2, bias=True)
+            self.conv3 = nn.Conv2d(32,  3, kernel_size=3, padding=1, bias=True)
             # self.conv3 = nn.Conv2d(32, 16, kernel_size=1, padding=0, bias=True)
             # self.conv4 = nn.Conv2d(16,  3, kernel_size=5, padding=2)
 
